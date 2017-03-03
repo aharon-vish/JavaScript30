da8a3c6a1d2dc585438945c2b160a8e5da401ca7
aharon-vish <aharon.vish@gmail.com>
2017-03-03 13:16:38 +0200

    third day

---
+++ 03 - CSS Variables/myScript.js
@@ -0,0 +1,13 @@
+/**
+ * Created by aharon on 3/3/17.
+ */
+const inputs = document.querySelectorAll('.controls input');
+
+function handelUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handelUpdate));
+
+ inputs.forEach(input => input.addEventListener('mousemove', handelUpdate));
